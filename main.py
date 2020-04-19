824069052b8041c0043d58ec92322e475ae32005
CLEMS3 <clement.chapard@gmail.com>
2020-04-19 15:38:46 +0200

    images importation

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ import csv
 import os
 import Soundex
 
-
 # window initialization
 pygame.init()
+
+# images importation
+def importation(image_name):
+    return pygame.image.load(str(image_name))
+
+bg = importation("src/menu_bg.png")
+tr = importation("src/Trophy.png")
+lg = importation("src/Statistics.png")
+
+
+# window setting
 win = pygame.display.set_mode((1000, 600))
-bg = pygame.image.load("src/menu_bg.png").convert()
-win.blit(bg, (0,0))
+win.blit(bg, (0, 0))
 
 # main loop
 _continue = True
@@ -23,4 +32,3 @@ while _continue:
             pygame.quit()
             os.sys.exit(0)
     pygame.display.flip()
-
