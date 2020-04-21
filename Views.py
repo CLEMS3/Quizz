09b8700f29316f8c415722eb5b59d31c112b9aff
CLEMS3 <clement.chapard@gmail.com>
2020-04-21 21:21:51 +0200

    adding the choice menu

--- Views.py
+++ Views.py
@@ -1,16 +1,30 @@
 # Clément Chapard - QuizzNsi
 # This file contain the directory of differents graphics view of the game
 
-def main_menu(win, bg, tr, stat, lang_img, set, button):
-    win.blit(bg, (0, 0))
-    win.blit(tr, (30, 22))
-    win.blit(stat, (120, 22))
-    win.blit(lang_img, (210, 22))
-    win.blit(set, (300, 22))
-    win.blit(button, (300, 250))
+# importation of modules
+import pygame
 
-def choice_menu():
-    pass
+def main_menu(win, bg, tr, stat, lang_img, set, button, user_view):
+    background = win.blit(bg, (0, 0))
+    scores_b = win.blit(tr, (30, 22))
+    stat_b = win.blit(stat, (120, 22))
+    lang_b = win.blit(lang_img, (210, 22))
+    settings_b = win.blit(set, (300, 22))
+    play_b = win.blit(button, (300, 250))
+
+    if play_b.collidepoint(pygame.mouse.get_pos()):
+        user_view == 2
+
+    return user_view
+#ne marche pas, utiliser enum
+
+
+
+def choice_menu(win, bg, nmb, klb, sdb):
+    win.blit(bg, (0,0))
+    win.blit(nmb, (300, 100))
+    win.blit(klb, (300, 250))
+    win.blit(sdb, (300, 400))
 
 def game():
     pass
--- main.py
+++ main.py
@@ -27,18 +27,39 @@ tr_img = conv_icone_size(importation("src/Trophy.png"))
 stat_img = conv_icone_size(importation("src/Statistics.png"))
 lang_img = conv_icone_size(importation("src/Language.png"))
 settings_img = conv_icone_size(importation("src/Settings.png"))
-play_button = importation("src/fr-buttons/play_button.png") if lang == "fr" else importation("src/en-buttons/play_button.png")
+general_bg = importation("src/bg.png")
+
+if lang == "fr":
+    play_button = importation("src/fr-buttons/play_button.png")
+    knowledge_button = importation("src/fr-buttons/knowledge_button.png")
+    normal_button = importation("src/fr-buttons/normal_button.png")
+    speed_button = importation("src/fr-buttons/speed_button.png")
+
+else:
+    play_button = importation("src/en-buttons/play_button.png")
+    knowledge_button = importation("src/en-buttons/knowledge_button.png")
+    normal_button = importation("src/en-buttons/normal_button.png")
+    speed_button = importation("src/en-buttons/speed_button.png")
 
 # window setting
 win = pygame.display.set_mode((1000, 600))
-main_menu(win=win, bg=bg_main_menu_img, tr=tr_img, stat=stat_img, lang_img=lang_img, set=settings_img, button=play_button)
+pygame.display.set_caption("E-quizz")
 
 # main loop
+user_view = 1
 _continue = True
 while _continue:
     for i in pygame.event.get():
+        print(user_view)
         key = pygame.key.get_pressed()
         if key[pygame.K_ESCAPE] or i.type == pygame.QUIT:
             pygame.quit()
             os.sys.exit(0)
+        if user_view == 1:
+            main_menu(win=win, bg=bg_main_menu_img, tr=tr_img, stat=stat_img, lang_img=lang_img, set=settings_img,
+                      button=play_button, user_view=user_view)
+        elif user_view == 2:
+            choice_menu(win=win, bg=general_bg, klb=knowledge_button, nmb=normal_button, sdb=speed_button)
+
+
     pygame.display.flip()
