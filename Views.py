24f75ab4cee4c7a56ee1201525818cfa4f800e71
CLEMS3 <clement.chapard@gmail.com>
2020-04-21 18:00:04 +0200

    adding views file + creating main menu

---
+++ Views.py
@@ -0,0 +1,31 @@
+# Clément Chapard - QuizzNsi
+# This file contain the directory of differents graphics view of the game
+
+def main_menu(win, bg, tr, stat, lang_img, set, button):
+    win.blit(bg, (0, 0))
+    win.blit(tr, (30, 22))
+    win.blit(stat, (120, 22))
+    win.blit(lang_img, (210, 22))
+    win.blit(set, (300, 22))
+    win.blit(button, (300, 250))
+
+def choice_menu():
+    pass
+
+def game():
+    pass
+
+def best_sores():
+    pass
+
+def statistics():
+    pass
+
+def language_choice():
+    pass
+
+def settings():
+    pass
+
+
+
--- main.py
+++ main.py
@@ -6,22 +6,32 @@ import pygame
 import csv
 import os
 import Soundex
+from Views import *
 
 # window initialization
 pygame.init()
+
 
 # images importation
 def importation(image_name):
     return pygame.image.load(str(image_name))
 
-bg = importation("src/menu_bg.png")
-tr = importation("src/Trophy.png")
-lg = importation("src/Statistics.png")
 
+def conv_icone_size(image):
+    return pygame.transform.scale(image, (50, 50))
+
+
+lang = "fr"
+bg_main_menu_img = importation("src/menu_bg.png")
+tr_img = conv_icone_size(importation("src/Trophy.png"))
+stat_img = conv_icone_size(importation("src/Statistics.png"))
+lang_img = conv_icone_size(importation("src/Language.png"))
+settings_img = conv_icone_size(importation("src/Settings.png"))
+play_button = importation("src/fr-buttons/play_button.png") if lang == "fr" else importation("src/en-buttons/play_button.png")
 
 # window setting
 win = pygame.display.set_mode((1000, 600))
-win.blit(bg, (0, 0))
+main_menu(win=win, bg=bg_main_menu_img, tr=tr_img, stat=stat_img, lang_img=lang_img, set=settings_img, button=play_button)
 
 # main loop
 _continue = True
